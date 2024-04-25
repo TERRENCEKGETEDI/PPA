b5c69a12a8f016d32a4db4181b24ccdbdfbc23df
Terrence Kgetedi <142272318+TERRENCEKGETEDI@users.noreply.github.com>
2024-04-25 02:04:35 +0200

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,18 @@
+function home(){
+    window.location.href = "#";
+}
+function Q1(){
+    window.location.href = "html/quiz1.html";
+}
+function documents(){
+    window.location.href = "html/documents.html";
+}
+function notes(){
+    window.location.href = "html/notes.html";
+}
+function quizzes(){
+    window.location.href = "html/quizzes.html";
+}
+function videos(){
+    window.location.href = "html/videos.html";
+}
