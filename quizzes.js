3f333ac4833a0ba8c48ae4d24f639f3772be8405
Terrence Kgetedi <142272318+TERRENCEKGETEDI@users.noreply.github.com>
2024-05-15 19:53:39 +0200

    Update quizzes.js

--- quizzes.js
+++ quizzes.js
@@ -111,3 +111,88 @@ function correction15(){
     hide1.style.display="none";
     view1.style.display="block";
 }
+
+function correction6(){
+    let correct1 = document.getElementById("correct6");
+    let hide1 = document.getElementById("hide6");
+    let view1 = document.getElementById("view6");
+    correct1.style.display="block";
+    hide1.style.display="block";
+    view1.style.display="none";
+}
+function correction16(){
+    let correct1 = document.getElementById("correct6");
+    let hide1 = document.getElementById("hide6");
+    let view1 = document.getElementById("view6");
+    correct1.style.display="none";
+    hide1.style.display="none";
+    view1.style.display="block";
+}
+
+function correction7(){
+    let correct1 = document.getElementById("correct7");
+    let hide1 = document.getElementById("hide7");
+    let view1 = document.getElementById("view7");
+    correct1.style.display="block";
+    hide1.style.display="block";
+    view1.style.display="none";
+}
+function correction17(){
+    let correct1 = document.getElementById("correct7");
+    let hide1 = document.getElementById("hide7");
+    let view1 = document.getElementById("view7");
+    correct1.style.display="none";
+    hide1.style.display="none";
+    view1.style.display="block";
+}
+
+function correction8(){
+    let correct1 = document.getElementById("correct8");
+    let hide1 = document.getElementById("hide8");
+    let view1 = document.getElementById("view8");
+    correct1.style.display="block";
+    hide1.style.display="block";
+    view1.style.display="none";
+}
+function correction18(){
+    let correct1 = document.getElementById("correct8");
+    let hide1 = document.getElementById("hide8");
+    let view1 = document.getElementById("view8");
+    correct1.style.display="none";
+    hide1.style.display="none";
+    view1.style.display="block";
+}
+
+function correction9(){
+    let correct1 = document.getElementById("correct9");
+    let hide1 = document.getElementById("hide9");
+    let view1 = document.getElementById("view9");
+    correct1.style.display="block";
+    hide1.style.display="block";
+    view1.style.display="none";
+}
+function correction19(){
+    let correct1 = document.getElementById("correct9");
+    let hide1 = document.getElementById("hide9");
+    let view1 = document.getElementById("view9");
+    correct1.style.display="none";
+    hide1.style.display="none";
+    view1.style.display="block";
+}
+
+function correction10(){
+    let correct1 = document.getElementById("correct5");
+    let hide1 = document.getElementById("hide5");
+    let view1 = document.getElementById("view5");
+    correct1.style.display="block";
+    hide1.style.display="block";
+    view1.style.display="none";
+}
+function correction110(){
+    let correct1 = document.getElementById("correct5");
+    let hide1 = document.getElementById("hide5");
+    let view1 = document.getElementById("view5");
+    correct1.style.display="none";
+    hide1.style.display="none";
+    view1.style.display="block";
+}
